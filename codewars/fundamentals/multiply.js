3e2b7fc53e7ca66699b2f201cb33978495f0d95b
Lamar Glenn <lvmvr.x@gmail.com>
2016-05-24 08:58:00 -0400

    Add multiply solution

---
+++ codewars/fundamentals/multiply.js
@@ -0,0 +1,17 @@
+/*
+========================
+		*** CODEWARS ***
+========================
+*/
+
+// Q: The code does not execute properly. Try to figure out why.
+function multiply(a, b){
+  a * b
+}
+
+//A: Add a return statement, and obligatory semicolon (I guess), call if you want. Run test if you choose.
+function multiply(a, b){
+  return a * b;
+}
+
+multiply(7, 7);
