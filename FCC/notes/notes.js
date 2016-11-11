e54e0f8a3ef4081080998759b8125300880f6b06
Lamar Glenn <lvmvr.x@gmail.com>
2016-11-11 11:45:38 -0500

    Add notes dir 📓

---
+++ FCC/notes/notes.js
@@ -0,0 +1,12 @@
+/*
+Notes for FreeCodeCamp (since i really fucking lost my notebook at the airport, like dumbass...)
+*/
+
+
+/*
+**************************
+
+ Object Oriented JS Notes
+
+**************************
+*/
