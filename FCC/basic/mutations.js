5a55ac734af87e8ae5570e721137021a577aa0fb
lvmvr <lvmvr.x@gmail.com>
2017-06-09 15:15:27 -0400

    Add mutations solution after 3 months 😓

---
+++ FCC/basic/mutations.js
@@ -0,0 +1,35 @@
+/*****************************
+* FreeCodeCamp solutions 
+******************************
+
+Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
+
+For example, ["hello", "Hello"], should return true because all of the letters in the second string are present in the first, ignoring case.
+
+The arguments ["hello", "hey"] should return false because the string "hello" does not contain a "y".
+
+Lastly, ["Alien", "line"], should return true because all of the letters in "line" are present in "Alien".
+*/
+
+
+function mutation(arr) {
+  // Let's try spliting the array into two seperate ones to compare.
+  var first = arr[0].toLowerCase();
+  var second = arr[1].toLowerCase();
+  var count = 0;
+  
+  for (var i = 0; i < second.length; i++) {
+    if (first.indexOf(second[i]) === -1) {
+      count++;
+    }
+  }
+  return count > 0 ? false : true;
+}
+
+mutation(["hello", "hey"]);
+
+
+
+
+
+
