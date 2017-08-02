744176e8e6037b1d20f5acb164945de94b19b545
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-02 12:36:38 -0400

    Add equalsTen solution 💵

---
+++ hack-reactor-prep/mod_1/equalsTen.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "equalsTen".
+
+Given a number, "equalsTen" returns whether or not the given number is 10.
+
+var output = equalsTen(9);
+console.log(output); // --> false
+
+Starter Code :
+function equalsTen(num) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function equalsTen(num) {
+  // your code here
+  return num === 10 ? true : false;
+}
