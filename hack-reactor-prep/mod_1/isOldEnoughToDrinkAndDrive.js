6ce33409a025085284b59553604c70d6299acef2
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-02 12:10:35 -0400

    Add function isOldEnoughToDrinkAndDrive(age) {

---
+++ hack-reactor-prep/mod_1/isOldEnoughToDrinkAndDrive.js
@@ -0,0 +1,24 @@
+/*
+Instructions from your teacher:
+Write a function called "isOldEnoughToDrinkAndDrive".
+
+Given a number, in this case an age, "isOldEnoughToDrinkAndDrive" returns whether a person of this given age is old enough to legally drink and drive in the United States.
+
+Notes:
+* The legal drinking age in the United States is 21.
+* It is always illegal to drink and drive in the United States.
+
+var output = isOldEnoughToDrinkAndDrive(22);
+console.log(output); // --> false
+
+Starter Code :
+function isOldEnoughToDrinkAndDrive(age) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function isOldEnoughToDrinkAndDrive(age) {
+  // your code here
+  return false;
+}
