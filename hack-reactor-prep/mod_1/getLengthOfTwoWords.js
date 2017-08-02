0d698a0cd196d17fd061089cbd62a2d13f52d961
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-02 12:27:41 -0400

    Add getLengthOfTwoWords solution 📏📏

---
+++ hack-reactor-prep/mod_1/getLengthOfTwoWords.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "getLengthOfTwoWords".
+
+Given 2 words, "getLengthOfTwoWords" returns the sum of their lengths.
+
+var output = getLengthOfTwoWords('some', 'words');
+console.log(output); // --> 9
+
+Starter Code :
+function getLengthOfTwoWords(word1, word2) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function getLengthOfTwoWords(word1, word2) {
+  // your code here
+  return word1.length + word2.length;
+}
