cdfacdfd46c345163d534d426e0febd2c2e2cc78
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-02 12:42:44 -0400

    Add isOdd solution  🖕🏾

---
+++ hack-reactor-prep/mod_1/isOdd.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "isOdd".
+
+Given a number, "isOdd" returns whether the given number is odd.
+
+var output = isOdd(9);
+console.log(output); // --> true
+
+Starter Code :
+function isOdd(num) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function isOdd(num) {
+  // your code here
+  return num % 2 !== 0 ? true : false;
+}
