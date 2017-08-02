8f4f05b0fc97369e94e70e161aa0fb9b1db7a9cf
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-02 12:41:47 -0400

    Add isEven solution 🖖🏾

---
+++ hack-reactor-prep/mod_1/isEven.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "isEven".
+
+Given a number, "isEven" returns whether it is even.
+
+var output = isEven(11);
+console.log(output); // --> false
+
+Starter Code :
+function isEven(num) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function isEven(num) {
+  // your code here
+  return num % 2 === 0 ? true : false;
+}
