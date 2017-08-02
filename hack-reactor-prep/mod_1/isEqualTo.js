c8d5ad6123962d4d25dd02800567340babd4ace1
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-02 12:40:35 -0400

    Add isEqualTo solution 💅

---
+++ hack-reactor-prep/mod_1/isEqualTo.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "isEqualTo".
+
+Given 2 numbers, "isEqualTo" returns whether num2 is equal to num1.
+
+var output = isEqualTo(11, 10);
+console.log(output); // --> false
+
+Starter Code :
+function isEqualTo(num1, num2) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function isEqualTo(num1, num2) {
+  // your code here
+  return num2 === num1 ? true : false;
+}
