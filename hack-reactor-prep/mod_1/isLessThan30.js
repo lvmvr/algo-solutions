8e920e9fb85e82aab8bce6789d9dfb5a0f88eb2b
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-02 12:32:36 -0400

    Add isLessThan30 solution 🙃

---
+++ hack-reactor-prep/mod_1/isLessThan30.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "isLessThan30".
+
+Given a number, "isLessThan30" returns whether the given number is less than 30.
+
+var output = isLessThan30(9);
+console.log(output); // --> true
+
+Starter Code :
+function isLessThan30(num) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function isLessThan30(num) {
+  // your code here
+  return num < 30 ? true : false;
+}
