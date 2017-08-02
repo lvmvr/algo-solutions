bedf3be5d2f996fed938e119c5f57d3caec82a80
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-02 12:24:35 -0400

    Add getFullName solution ✍🏽

---
+++ hack-reactor-prep/mod_1/getFullName.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "getFullName".
+
+Given a first and a last name, "getFullName" returns a single string with the given first and last names separated by a single space.
+
+var output = getFullName('Joe', 'Smith');
+console.log(output); // --> 'Joe Smith'
+
+Starter Code :
+function getFullName(firstName, lastName) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function getFullName(firstName, lastName) {
+  // your code here
+  return firstName + " " + lastName;
+}
