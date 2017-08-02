a2dd655b86a76b85d40eb079e6ef022a830aeb8f
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-02 12:29:17 -0400

    Add isGreaterThanTen solution 💪🏾

---
+++ hack-reactor-prep/mod_1/isGreaterThanTen.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "isGreaterThanTen".
+
+Given a number, "isGreaterThanTen" returns whether it is greater than 10.
+
+var output = isGreaterThanTen(11);
+console.log(output); // --> true
+
+Starter Code :
+function isGreaterThanTen(num) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function isGreaterThanTen(num) {
+  // your code here
+  return num > 10 ? true : false;
+}
