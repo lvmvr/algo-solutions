96689b5c01dfe05b8fc0acfdd03cc9330637d91f
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-02 12:25:47 -0400

    Add getLengthOfWord solution 📏

---
+++ hack-reactor-prep/mod_1/getLengthOfWord.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "getLengthOfWord".
+
+Given a word, "getLengthOfWord" returns the length of the given word.
+
+var output = getLengthOfWord('some');
+console.log(output); // --> 4
+
+Starter Code :
+function getLengthOfWord(word) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function getLengthOfWord(word) {
+  // your code here
+  return word.length;
+}
