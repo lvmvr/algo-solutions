98e45fd11e0d553fadc2e6906d32b5f090b33caa
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-02 12:38:01 -0400

    Add isLessThan solution 👈🏾

---
+++ hack-reactor-prep/mod_1/isLessThan.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "isLessThan".
+
+Given 2 numbers, "isLessThan" returns whether num2 is less than num1.
+
+var output = isLessThan(9, 4);
+console.log(output); // --> true
+
+Starter Code :
+function isLessThan(num1, num2) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function isLessThan(num1, num2) {
+  // your code here
+  return num2 < num1 ? true : false;
+}
