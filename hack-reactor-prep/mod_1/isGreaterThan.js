267f4ee70e667e337c7c4a8cbf15d2e6f3c16dc6
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-02 12:39:11 -0400

    Add isGreaterThan solution 👉🏾

---
+++ hack-reactor-prep/mod_1/isGreaterThan.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "isGreaterThan".
+
+Given 2 numbers, "isGreaterThan" returns whether num2 is greater than num1.
+
+var output = isGreaterThan(11, 10);
+console.log(output); // --> false
+
+Starter Code :
+function isGreaterThan(num1, num2) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function isGreaterThan(num1, num2) {
+  // your code here
+  return num2 > num1 ? true : false;
+}
