b71ea4501a39b831ba68e5f718163fce6586170e
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-03 22:48:28 -0400

    Add isSameLength solution 📏😴

---
+++ hack-reactor-prep/mod_1/isSameLength.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "isSameLength".
+
+Given two words, "isSameLength" returns whether the given words have the same length.
+
+var output = isSameLength('words', 'super');
+console.log(output); // --> true
+
+Starter Code : 
+function isSameLength(word1, word2) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function isSameLength(word1, word2) {
+  // your code here
+  return word1.length === word2.length ? true : false;
+} 
