30262d01c0bf7abaca0d842d984a192d946866d0
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-03 23:04:11 -0400

    Add addFullNameProperty solution 🤤

---
+++ hack-reactor-prep/mod_1/addFullNameProperty.js
@@ -0,0 +1,24 @@
+/*
+Instructions from your teacher:
+Write a function called "addFullNameProperty".
+
+Given an object that has a "firstName" property and a "lastName" property, "addFullNameProperty" returns a "fullName" property whose value is a string with the first name and last name separated by a space.
+
+var person = {
+  firstName: 'Jade',
+  lastName: 'Smith'
+};
+addFullNameProperty(person);
+console.log(person.fullName); // --> 'Jade Smith'
+
+Starter Code :
+function addFullNameProperty(obj) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function addFullNameProperty(obj) {
+  // your code here
+  obj.fullName = obj.firstName + " " + obj.lastName;
+}
