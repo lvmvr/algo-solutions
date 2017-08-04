11ede2be6cf1b92eb764bcbc75b5e38da75bd158
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-03 22:57:30 -0400

    Add average solution 💩

---
+++ hack-reactor-prep/mod_1/average.js
@@ -0,0 +1,23 @@
+/*
+Instructions from your teacher:
+Write a function called "average".
+
+Given two numbers, "average" returns their average.
+
+var output = average(4, 6);
+console.log(output); // --> 5
+
+Starter Code :
+function average(num1, num2) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function average(num1, num2) {
+  // your code here 
+  var sum = num1 + num2;
+  return sum / 2;
+}
+
+average(5, 9);
