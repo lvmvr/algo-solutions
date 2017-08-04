2842710d263c0d408ccae87ef9fba0ffe1b2ab8f
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-03 22:58:32 -0400

    Add computeAreaOfATriangle solution 🙌🏾

---
+++ hack-reactor-prep/mod_1/computeAreaOfATriangle.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "computeAreaOfATriangle".
+
+Given the base and height of a triangle, "computeAreaOfATriangle" returns its area.
+
+var output = computeAreaOfATriangle(4, 6);
+console.log(output); // --> 12
+
+Starter Code :
+function computeAreaOfATriangle(base, height) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function computeAreaOfATriangle(base, height) {
+  // your code here
+  return base * height / 2;
+}
