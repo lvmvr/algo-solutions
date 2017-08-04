dd13e94ec85ffbdf3cb263a1685bd303e7788238
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-03 22:59:25 -0400

    Add cube solution 🖕🏾🖕🏾🖕🏾

---
+++ hack-reactor-prep/mod_1/cube.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "cube".
+
+Given a number, "cube" returns the cube of that number.
+
+var output = cube(3);
+console.log(output); // --> 27
+
+Starter Code :
+function cube(num) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function cube(num) {
+  // your code here
+  return num * num * num;
+}w
