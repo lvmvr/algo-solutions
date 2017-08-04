d8acb5558e47a89296246ff0bb3482051f50a7ac
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-03 22:54:00 -0400

    Bad commit #1 shame... 😅

--- hack-reactor-prep/mod_1/isOddLength.js
+++ hack-reactor-prep/mod_1/isOddLength.js
@@ -1,5 +1,20 @@
 /*
+Instructions from your teacher:
+Write a function called "isOddLength".
 
+Given a word, "isOddLength" returns whether the length of the given word is odd.
+
+var output = isOddLength('special');
+console.log(output); // --> true
+
+Starter Code :
+function isOddLength(word) {
+  // your code here
+}
 */
 
 /*SOLUTION*/
+function isOddLength(word) {
+  // your code here
+  return word.length % 2 !== 0 ? true : false;
+}
