8f641f9be99db1ed622ac068074d396b06df2bd9
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-03 23:01:31 -0400

    Add square solution ✅

---
+++ hack-reactor-prep/mod_1/square.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "square".
+
+Given a number, "square" should return the square of the given number.
+
+var output = square(5);
+console.log(output); // --> 25
+
+Starter Code :
+function square(num) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function square(num) {
+  // your code here
+  return num * num;
+}
