b0eda8e67edbc573da900177b6c52199f6315035
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-28 11:36:20 -0400

    Add computePerimeterOfACircle solution

---
+++ hack-reactor-prep/mod_1/computePerimeterOfACircle.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "computePerimeterOfACircle".
+
+Given the radius of a circle, "computePerimeterOfACircle" returns its perimeter.
+
+var output = computePerimeterOfACircle(4);
+console.log(output); // --> 25.132741228718345
+
+Starter Code :
+function computePerimeterOfACircle(radius) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function computePerimeterOfACircle(radius) {
+  // your code here
+  return (radius * 2) * Math.PI;
+}
