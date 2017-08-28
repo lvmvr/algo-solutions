6f3e51df34cf81fd71fa6022c05d96687881b8b8
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-28 11:37:09 -0400

    Add computePerimeterOfARectangle solution

---
+++ hack-reactor-prep/mod_1/computePerimeterOfARectangle.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "computePerimeterOfARectangle".
+
+Given a length and a width describing a rectangle, "computePerimeterOfARectangle" returns its perimter.
+
+var output = computePerimeterOfARectangle(5, 2);
+console.log(output); // --> 14
+
+Starter Code :
+function computePerimeterOfARectangle(length, width) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function computePerimeterOfARectangle(length, width) {
+  // your code here
+  return (length + width) * 2;
+}
