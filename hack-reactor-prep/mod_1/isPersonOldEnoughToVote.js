8ae0b45a151b9b2400a03b1b8b20add7ebf2b1d6
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-28 11:42:21 -0400

    Add isPersonOldEnoughToVote solution

---
+++ hack-reactor-prep/mod_1/isPersonOldEnoughToVote.js
@@ -0,0 +1,26 @@
+/*
+Instructions from your teacher:
+Write a function called "isPersonOldEnoughToVote".
+
+Given a "person" object, that contains an "age" property, "isPersonOldEnoughToVote" returns whether the given person is old enough to vote.
+
+Notes:
+* The legal voting age in the United States is 18.
+
+var obj = {
+  age: 19
+};
+var output = isPersonOldEnoughToVote(obj);
+console.log(output); // --> true
+
+Starter Code :
+function isPersonOldEnoughToVote(person) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function isPersonOldEnoughToVote(person) {
+  // your code here
+  return person.age >= 18 ? true : false;
+}
