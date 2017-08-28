8f1e35b8086d85f2df8fb943ad8905cd046ad988
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-28 11:35:56 -0400

    Add computeAreaOfARectangle solution

---
+++ hack-reactor-prep/mod_1/computeAreaOfARectangle.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "computeAreaOfARectangle".
+
+Given the length and width of a rectangle, "computeAreaOfARectangle" returns its area.
+
+var output = computeAreaOfARectangle(4, 8);
+console.log(output); // --> 32
+
+Starter Code :
+function computeAreaOfARectangle(length, width) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function computeAreaOfARectangle(length, width) {
+  // your code here
+  return width * length;
+}
