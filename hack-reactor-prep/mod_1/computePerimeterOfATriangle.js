cbd034dafcc0f3d8094add60a66fa5cb782d6f74
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-28 11:37:33 -0400

    Add computePerimeterOfATriangle solution

---
+++ hack-reactor-prep/mod_1/computePerimeterOfATriangle.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "computePerimeterOfATriangle".
+
+Given 3 sides describing a triangle, "computePerimeterOfATriangle" returns its perimter.
+
+var output = computePerimeterOfATriangle(6, 4, 10);
+console.log(output); // --> 20
+
+Starter Code :
+function computePerimeterOfATriangle(side1, side2, side3) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function computePerimeterOfATriangle(side1, side2, side3) {
+  // your code here
+  return side1 + side2 + side3;
+}
