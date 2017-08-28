62a60ecce0f9219ef916576ae38b79f59a176bb8
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-28 11:38:14 -0400

    Add computeTripledAreaOfARectangle solution

---
+++ hack-reactor-prep/mod_1/computeTripledAreaOfARectangle.js
@@ -0,0 +1,20 @@
+/*
+Instructions from your teacher:
+Write a function called "computeTripledAreaOfARectangle".
+
+Given a length and width of a rectangle, "computeTripledAreaOfARectangle" returns the rectangle's area, multiplied by 3.
+
+var output = computeTripledAreaOfARectangle(2, 4);
+console.log(output); // --> 24
+
+Starter Code :
+function computeTripledAreaOfARectangle(length, width) {
+  // your code here
+}
+*/  
+
+/*SOLUTION*/
+function computeTripledAreaOfARectangle(length, width) {
+  // your code here
+  return (length * width) * 3;
+}
