668848fc54ac7fd03e377841956ea820d9a5c053
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-28 11:40:21 -0400

    Add isPersonOldEnoughToDrink solution

---
+++ hack-reactor-prep/mod_1/isPersonOldEnoughToDrink.js
@@ -0,0 +1,26 @@
+/*
+Instructions from your teacher:
+Write a function called "isPersonOldEnoughToDrink".
+
+Given a "person" object, that contains an "age" property, "isPersonOldEnoughToDrink" returns whether the given person is old enough to drink.
+
+Notes:
+* The legal drinking age in the United States is 21.
+
+var obj = {
+  age: 16
+};
+var output = isPersonOldEnoughToDrink(obj);
+console.log(output); // --> false
+
+Starter Code :
+function isPersonOldEnoughToDrink(person) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function isPersonOldEnoughToDrink(person) {
+  // your code here
+  return person.age >= 21 ? true : false;
+}
