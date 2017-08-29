edd7c77a86387849ebb517e7150f76e0c8a726b9
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-29 14:44:24 -0400

    Add removeFromFront solution.

---
+++ hack-reactor-prep/mod_1/removeFromFront.js
@@ -0,0 +1,25 @@
+/*
+Unsubmit
+Instructions from your teacher:
+Write a function called "removeFromFront".
+
+Given an array, "removeFromFront" returns the given array with its first element removed.
+
+Notes:
+* You should be familiar with the method 'shift'.
+
+var output = removeFromFront([1, 2, 3]);
+console.log(output); // --> [2, 3]
+
+Starter Code :
+function removeFromFront(arr) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function removeFromFront(arr) {
+  // your code here
+  arr.shift();
+  return arr;
+}
