da851362b5f9e6b1b6b43b9108f5938ca1b707ed
Lamar Glenn <lvmvr.x@gmail.com>
2017-08-30 07:52:28 -0400

    Add extend solution.

---
+++ hack-reactor-prep/mod_1/extend.js
@@ -0,0 +1,42 @@
+/*
+Instructions from your teacher:
+Write a function called "extend".
+
+Given two objects, "extend" adds properties from the 2nd object to the 1st object.
+
+Notes:
+* Add any keys that are not in the 1st object.
+* If the 1st object already has a given key, ignore it (do not overwrite the property value).
+* Do not modify the 2nd object at all.
+
+var obj1 = {
+  a: 1,
+  b: 2
+};
+var obj2 = {
+  b: 4,
+  c: 3
+};
+
+extend(obj1, obj2);
+
+console.log(obj1); // --> {a: 1, b: 2, c: 3}
+console.log(obj2); // --> {b: 4, c: 3}
+
+Starter Code :
+function extend(obj1, obj2) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function extend(obj1, obj2) {
+  // your code here
+  for (var i in obj2) {
+    if (obj1.hasOwnProperty(i)){
+      continue;
+    } else {
+      obj1[i] = obj2[i];
+    }
+  }
+}
