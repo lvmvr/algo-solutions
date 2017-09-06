89dbed944ee2c02d60c7953d0d3ba1fbec855071
Lamar Glenn <lvmvr.x@gmail.com>
2017-09-06 13:45:13 -0400

    Add areValidCredentials solution

---
+++ hack-reactor-prep/mod_1/areValidCredentials.js
@@ -0,0 +1,24 @@
+/*
+Instructions from your teacher:
+Write a function called "areValidCredentials".
+
+Given a name and a password, "areValidCredentials", returns true if the name is longer than 3 characters, AND, the password is at least 8 characters long. Otherwise it returns false.
+
+var output = areValidCredentials('Ritu', 'mylongpassword')
+console.log(output); // --> true
+
+Starter Code :
+function areValidCredentials(name, password) {
+  // your code here
+}
+*/
+
+/*SOLUTION*/
+function areValidCredentials(name, password) {
+  // your code here
+  if (name.length > 3 && password.length >= 8){
+    return true;
+  } else {
+    return false;
+  }
+}
